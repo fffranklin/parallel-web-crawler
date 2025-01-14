6748adf4d3927c011a6dea42bc249af8159828bc
Fernanda <fernanda.ffranklin@gmail.com>
2025-01-14 03:03:07 -0300

    Adding Step 3 - 'Legacy Crawler'

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -15,6 +15,7 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public final class WebCrawlerMain {
@@ -36,8 +37,14 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+
+    String resultPathString = config.getResultPath();
+    if (resultPathString != null && !resultPathString.isEmpty()) {
+      Path resultPath = Paths.get(resultPathString);
+      resultWriter.write(resultPath);
+    } else {
+      System.out.println("Error config.getResultPath() --> The file name is empty." + result);
+    }
   }
 
   public static void main(String[] args) throws Exception {
